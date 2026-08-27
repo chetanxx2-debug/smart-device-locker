4858b246fe02514366e91bacafc84bc9a33679b9
chetan aglave <chetan.xx2@gamil.com>
2026-08-27 19:19:50 +0530

    Deploy Production Signed Release APK and update 6-Tap QR with both cert and package checksums

--- js/seller.js
+++ js/seller.js
@@ -1286,9 +1286,10 @@ class SellerPortal {
 
         const payload = {
             "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME": "com.smartlocker.client/com.smartlocker.client.LockerAdminReceiver",
-            "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": apkUrl,
-            "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "s8xCFm8_umf3I25U24KnwWsUiQXkS-za9t3MqzLIraU",
             "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_NAME": "com.smartlocker.client",
+            "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": apkUrl,
+            "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "Yd8zcgqngK7qNlnG7KiTuKkU5IgIGn8BoEIxUuIXkxA",
+            "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "0ZKTl3Z5j2D4F7bDKr61L6NHP7CjyfF776b-5dIRjFU",
             "android.app.extra.PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED": true,
             "android.app.extra.PROVISIONING_SKIP_ENCRYPT": true,
             "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
